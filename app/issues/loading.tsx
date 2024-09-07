ae0d3ce8b6a9adde09115642a7f6026ac119d265
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-09-07 22:07:28 +0200

    19. Add issues loading skeleton.

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,12 +1,10 @@
-import prisma from '@/prisma/client';
 import { Table } from '@radix-ui/themes';
-import delay from 'delay';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import IssueActions from '../components/IssueActions';
-import IssueStatusBadge from '../components/IssueStatusBadge';
 
-const page = async () => {
-  const issues = await prisma.issue.findMany();
-  await delay(2000);
+const loading = () => {
+  const issues = [1, 2, 3, 4, 5];
 
   return (
     <div>
@@ -27,20 +25,20 @@ const page = async () => {
 
         <Table.Body>
           {issues.map((issue) => (
-            <Table.Row key={issue.id}>
+            <Table.Row key={issue}>
               <Table.Cell>
-                {issue.title}
+                <Skeleton />
                 <div className='block md:hidden'>
-                  <IssueStatusBadge status={issue.status} />
+                  <Skeleton />
                 </div>
               </Table.Cell>
 
               <Table.Cell className='hidden md:table-cell'>
-                <IssueStatusBadge status={issue.status} />
+                <Skeleton />
               </Table.Cell>
 
               <Table.Cell className='hidden md:table-cell'>
-                {issue.createdAt.toDateString()}
+                <Skeleton />
               </Table.Cell>
             </Table.Row>
           ))}
@@ -50,4 +48,4 @@ const page = async () => {
   );
 };
 
-export default page;
+export default loading;
