0389f0709729c3a8ea8964b46449167fe4507d21
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-09-14 22:12:36 +0200

    50. Protect the routes and endpoints against unauthenticated user.

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -2,8 +2,13 @@ import { NextRequest, NextResponse } from 'next/server';
 import { z } from 'zod';
 import prisma from '@/prisma/client';
 import { issueSchema } from '@/app/validationSchemas';
+import { getServerSession } from 'next-auth';
+import AuthOptions from '@/app/auth/AuthOptions';
 
 export async function POST(request: NextRequest) {
+  const session = await getServerSession(AuthOptions);
+  if (!session) return Response.json({}, { status: 401 });
+
   const body = await request.json();
 
   const validation = issueSchema.safeParse(body);
