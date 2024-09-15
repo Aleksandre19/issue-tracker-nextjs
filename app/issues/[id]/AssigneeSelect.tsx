745bd25851a40d7022d74c65044ed29280dff0a9
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-09-16 00:53:05 +0200

    57. Assign an issue to a user.

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import { Skeleton, Spinner } from '@/app/components';
-import { User } from '@prisma/client';
+import { Issue, User } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
     data: users,
     error,
@@ -23,11 +23,20 @@ const AssigneeSelect = () => {
   if (error) return null;
 
   return (
-    <Select.Root>
+    <Select.Root
+      defaultValue={issue.assignedToUserId || 'unassigned'}
+      onValueChange={(userId) => {
+        const assignedId = userId === 'unassigned' ? null : userId;
+        axios.patch(`/api/issues/${issue.id}`, {
+          assignedToUserId: assignedId,
+        });
+      }}
+    >
       <Select.Trigger placeholder='Assign...' />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
+          <Select.Item value='unassigned'>Unassigned</Select.Item>
           {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
