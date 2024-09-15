976ef2854c5586b9f7ae296b9e7f9e06d5e4c249
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-09-16 01:28:17 +0200

    60. Build the filter component.

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,6 +1,6 @@
 export { default as Link } from './Link';
 export { default as IssueStatusBadge } from './IssueStatusBadge';
 export { default as Spinner } from './Spinner';
-export { default as IssueActions } from './IssueActions';
+export { default as IssueActions } from '../issues/list/IssueActions';
 export { default as ErrorMessage } from './ErrorMessage';
 export { default as Skeleton } from './Skeleton';
