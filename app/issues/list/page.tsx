3272e90d0060efeba36a55c1b8aa25171226f5fd
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-09-16 23:58:33 +0200

    61. Filter the issues by status.

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,9 +1,23 @@
 import prisma from '@/prisma/client';
 import { Table } from '@radix-ui/themes';
 import { Link, IssueActions, IssueStatusBadge } from '@/app/components';
+import { Status } from '@prisma/client';
 
-const IssuePage = async () => {
-  const issues = await prisma.issue.findMany();
+interface Props {
+  searchParams: { status: Status };
+}
+
+const IssuePage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: {
+      status: status,
+    },
+  });
 
   return (
     <div>
